ea68cea47f9612d0a38fed9d5dc60f7714b91c2b
AbiyaMakruf <aabbiiyyaa@gmail.com>
2023-07-29 13:47:58 +0700

    Add requirements

--- app.py
+++ app.py
@@ -30,7 +30,8 @@ def hello():
 
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=5000, debug=True)
+    #app.run(host="localhost", port=5000, debug=True)
+    app.run()
 
 # Repositories
 from Infrastructure.repository import (
